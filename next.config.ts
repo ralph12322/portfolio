1b68b5c6cafbf3f9619a4988f68b823c67505497
Ralph Geo Santos <gjcshs.santos.ralphgeo@gmail.com>
2026-03-07 22:25:34 +0800

    fix audio content-type headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/music/:file*.mp3',
+        headers: [{ key: 'Content-Type', value: 'audio/mpeg' }],
+      },
+      {
+        source: '/music/:file*.mp4',
+        headers: [{ key: 'Content-Type', value: 'video/mp4' }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
